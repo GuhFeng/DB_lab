0724001d071cd98b87eef0ac4862d193ba972005
fgh <fenguhao@stu.pku.edu.cn>
2023-05-28 14:28:39 +0800

    ok

--- SQL/tmp.sql
+++ SQL/tmp.sql
@@ -23,4 +23,7 @@ WHERE [Followed ID]=1 and [Following ID]=1;
 INSERT INTO [User]
     ([User ID],[User Name],[Creating Time],[Password])
 VALUES
-    (103, '1', '2036-04-13 18:55:22', '2'); 
+    (103, '1', '2036-04-13 18:55:22', '2');
+
+SELECT *
+from [Post Content];
